ef163f7716b7fc1e42b909cdb88352ff64b0ca76
Zaupower <56695737+Zaupower@users.noreply.github.com>
2023-02-16 10:50:24 +0000

    Add project files.

---
+++ DelegatePerson.cs
@@ -0,0 +1,26 @@
+﻿    public class DelegatePerson
+    {
+        //Delegates work has callbacks 
+        //They can be declared in on class and call functions defined on another classes
+        //Func is a delegate that receives two input parameters and one output : Func<input1, input2, output>
+        //Action is a delegate that can receives up to 16 input parameters but return allways void
+        public delegate string DelegatesGretings(string gretingFirstName, string gretingLastName, int gretingAge);
+
+        public string Gretings(DelegatesGretings gretings, Person person)
+        {
+            return gretings(person.FirstName, person.LastName, person.Age);
+        }
+    }
+
+
+//Implement Where
+//public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+//{
+//    foreach (T item in source)
+//    {
+//        if (predicate(item))
+//        {
+//            yield return item;
+//        }
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DelegatePerson delegatePerson = new DelegatePerson();
+        Person person = new Person
+        {
+            Age = 18,
+            FirstName = "Marcelo",
+            LastName = "Carvalho"
+        };
+
+        Console.WriteLine(delegatePerson.Gretings(PersonalizedGretings, person) );   
+
+    }
+
+    public static string PersonalizedGretings(string firsName, string lastName, int age)
+    {
+        string gretingFirstName = " Sr. ";
+        string gretingLastName = "Mo";
+        return gretingFirstName+ firsName+", "+ gretingLastName+" "+ lastName+ " have "+ age+" springs";  
+    }
+}
+
